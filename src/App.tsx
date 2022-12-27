6064beb1190719826c1d8719bc8e2b9ac6ccdd82
Mason Pike <masonapike@gmail.com>
2022-12-27 22:15:59 +0100

    axios and cors complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import './styles/App.scss';
+import axios from 'axios';
+
+const url = "localhost%38080/api/groups";
+const POST_OPTIONS = {
+  method: 'POST',
+  url: url,
+  data: {
+    name: "four big guys",
+    city: "here",
+    country: "myass"
+  }
+}
 
 
 function Blog(): JSX.Element {
 
   const [groups, setGroups] = useState<any>([]);
   const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     setLoading(true);
@@ -23,6 +36,22 @@ function Blog(): JSX.Element {
     return <p>Loading...</p>;
   }
 
+  function handleChange(event: ChangeEvent<HTMLInputElement>): void {
+    setInput(event.target.value);
+  }
+
+
+  async function handleSubmit(event: { preventDefault: () => void; }): Promise<void> {
+    event.preventDefault();
+    axios.get('http://localhost:8080/api/groups')
+      .then(response => { if (response.status === 200) {
+        axios.post("http://localhost:8080/api/group", {
+          name: "it do be workin tho..."
+        })
+      }})
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -34,6 +63,12 @@ function Blog(): JSX.Element {
               </div>
             )
           }
+        </div>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="input" id="0" onChange={() => handleChange} />
+            <input type="submit" value="submit" />
+          </form>
         </div>
       </header>
     </div>
